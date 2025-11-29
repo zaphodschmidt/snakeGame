fbd6b69c6d0f2b61b46b564d8b3fa0d97b0cd377
zaphodschmidt <51718047+zaphodschmidt@users.noreply.github.com>
2025-11-29 15:08:39 -0700

    Update asgi.py

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -5,6 +5,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+rheh
 """
 
 import os
